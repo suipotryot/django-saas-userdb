a5e8a4018b8c6ba4414198716dc13bdc04b2e7ec
Gus <guillaume1.dubus@gmail.com>
2017-02-09 15:55:35 +0100

    ADD setup files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # django-saas-userdb
 A lightweight application to use Django as SaaS (Software as a Service). It separates users databases for the same Django instance.
+
+## Installation
+
+```
+pip install django-saas-userdb
+```
+
+## Configuration
+
+In your settings: 
+```
+DATABASE_ROUTERS = ['poc_noe_saas.routers.AuthRouter', 'poc_noe_saas.routers.DatabaseRouter']
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = 'django-saas-userdb',
+    packages = ['django-saas-userdb'],
+    version = __import__('userdb').__version__,
+    author = 'Guillaume D',
+    author_email = 'guillaume1.dubus@gmail.com',
+    description = 'A lightweight application to use Django as SaaS (Software as a Service). It separates users databases for the same Django instance.',
+    license = 'MIT',
+    url = 'https://github.com/suipotryot/django-saas-userdb',
+    download_url = 'https://github.com/suipotryot/django-saas-userdb/tarball/0.1',
+    keywords = ['django', 'saas', 'user', 'database'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
