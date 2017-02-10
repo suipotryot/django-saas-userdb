d4ba36ebf2e61a42eac08cc3bca170456d179c3f
Gus <guillaume1.dubus@gmail.com>
2017-02-10 11:10:14 +0100

    Setup package for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name = 'django-saas-userdb',
     packages = ['userdb'],
     version = __import__('userdb').__version__,
+    description = 'A lightweight application to use Django as SaaS (Software as a Service). It separates users databases for the same Django instance.',
+    long_description = long_description,
     author = 'Guillaume D',
     author_email = 'guillaume1.dubus@gmail.com',
-    description = 'A lightweight application to use Django as SaaS (Software as a Service). It separates users databases for the same Django instance.',
     license = 'MIT',
     url = 'https://github.com/suipotryot/django-saas-userdb',
     download_url = 'https://github.com/suipotryot/django-saas-userdb/tarball/0.1',
